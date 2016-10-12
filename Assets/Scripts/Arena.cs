fe00be2640297caaa09b7e01bfe4b250f10122e4
Marcin Dzierwa <microscopes.team@gmail.com>
2016-10-12 14:31:17 +0200

    branch commit test

---
+++ Assets/Scripts/Arena.cs
@@ -0,0 +1,254 @@
+﻿/*!
+ * @file Arena.cs
+ * @brief Contains Arena class definition.
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Arena : MonoBehaviour 
+{
+	private Color[] pixMap;					// Table of texture pixel colors
+	private MeshRenderer renderer;			// Arena renderer component
+	private Texture2D texture;				// Arena texture
+
+	private int arenaSize;
+
+	private float frameRate;
+	private float nextFrame;
+
+	private int initSize = 4;
+	private float initSpeed = 2.0f;
+
+	private int noOfPlayers = 1;
+	private List<Player> players = new List<Player>();
+
+
+	// Use this for initialization
+	void Start() 
+	{		
+		frameRate 		= 0.01f;
+		arenaSize 		= 800;
+
+		renderer 	= GetComponent<MeshRenderer> ();
+		texture 	= new Texture2D (arenaSize,arenaSize);
+		pixMap 		= texture.GetPixels ();
+
+		texture.filterMode = FilterMode.Point;//Trilinear;
+
+		// Setting texture to Arena renderer
+		renderer.material.mainTexture = texture;
+
+		// Setting Arena background color
+		SetArenaBgColor (Color.black);
+
+		for (int i = 0; i < noOfPlayers; i++) {
+			players.Add (new Player ());
+		}
+
+		foreach (Player player in players) {
+			player.SetupPlayer(new Vector2 (Random.Range(20.0f, arenaSize-20), 
+											Random.Range(20.0f, arenaSize-20)), 
+											Random.Range(0.0f, 2.0f), 
+											initSize, 
+											initSpeed, 
+											Color.red);
+		}
+
+		//players [1].ChangeColor (Color.green);
+		//players [1].SetControlKeys (KeyCode.Z, KeyCode.X);
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Time.time > nextFrame) 
+		{
+			// Setting next frame delay
+			nextFrame = Time.time + frameRate;
+
+			foreach (Player player in players) 
+			{
+				player.Turn ();
+			}
+				
+			RedrawArena ();
+		}
+
+		if(Input.GetKeyDown(KeyCode.G))
+		{
+			players [0].DoubleSize ();
+		}
+
+		if(Input.GetKeyDown(KeyCode.H))
+		{
+			players [0].ReduceSize ();
+		}
+
+		if(Input.GetKeyDown(KeyCode.J))
+		{
+			players [0].DoubleSpeed ();
+		}
+
+		if(Input.GetKeyDown(KeyCode.K))
+		{
+			players [0].ReduceSpeed ();
+		}
+		// helpful for getting KeyCode of pressed key (to set preferred keys for players in GUI)
+
+		/*foreach(KeyCode kcode in System.Enum.GetValues(typeof(KeyCode)))
+		{
+			if (Input.GetKeyDown(kcode))
+				Debug.Log("KeyCode down: " + kcode);
+		}*/
+	}
+
+
+	void RedrawArena ()
+	{
+		DrawPlayers ();
+
+		texture.SetPixels (pixMap);
+		texture.Apply (false);
+	}
+
+
+	void DrawPlayers ()
+	{
+		foreach (Player player in players)
+		{
+			float playerPosX = player.GetX ();
+			float playerPosY = player.GetY ();
+
+			Color playerColor = player.GetColor ();
+
+			int playerSize = player.GetSize ();
+			float playerSpeed = player.GetSpeed ();
+
+			float sinDegree = Mathf.Sin (Mathf.PI * player.GetDegree ());
+			float cosDegree = Mathf.Cos (Mathf.PI * player.GetDegree ());
+
+			for (int j = 0; j < Mathf.CeilToInt(playerSpeed); j++) 
+			{
+				if (player.isActive ()) 
+				{
+					if (playerSpeed < 1) {
+						playerPosX += sinDegree * playerSpeed;
+						playerPosY += cosDegree * playerSpeed;
+					} else {
+						playerPosX += sinDegree;
+						playerPosY += cosDegree;
+					}
+
+					float posX = playerPosX - playerSize * cosDegree;
+					float posY = playerPosY + playerSize * sinDegree;
+
+					for (int i = 1; i < 2 * playerSize; i++) 
+					{
+						float posX2 = posX + i * playerSize * cosDegree / playerSize;
+						float posY2 = posY - i * playerSize * sinDegree / playerSize;
+
+						for (int k = 1; k <= Mathf.CeilToInt (playerSize * 0.5f) + 3; k++) {
+							DrawPixel (posX2 - sinDegree * k / 5.0f, posY2 - cosDegree * k / 5.0f, playerColor, true);
+						}
+
+					if ((i % 2 == 0 || i == 1) && DrawPixel (posX2 + 2.8f * sinDegree, posY2 + 2.8f * cosDegree, Color.green, false)) {
+							player.Collision ();
+						} 
+
+						DrawPixel (posX2, posY2, playerColor, true);
+					}
+				}
+			}
+
+			player.SetX (playerPosX);
+			player.SetY (playerPosY);
+		}
+	}
+
+
+	/*void DrawPlayers2 ()
+	{
+		foreach (Player player in players)
+		{
+			if (player.isActive ()) 
+			{
+				float playerPosX = player.GetX ();
+				float playerPosY = player.GetY ();
+
+				Color playerColor = player.GetColor ();
+
+				int playerSize = player.GetSize ();
+				float playerSpeed = player.GetSpeed ();
+
+				float sinDegree = Mathf.Sin (Mathf.PI * player.GetDegree ()) / (playerSize * 0.2f);
+				float cosDegree = Mathf.Cos (Mathf.PI * player.GetDegree ()) / (playerSize * 0.2f);
+
+				for (int j = 0; j < Mathf.CeilToInt(playerSpeed * playerSize * 0.1f); j++) 
+				{
+					playerPosX += sinDegree;
+					playerPosY += cosDegree;
+
+					float posX = playerPosX - playerSize * cosDegree * (playerSize * 0.2f);
+					float posY = playerPosY + playerSize * sinDegree * (playerSize * 0.2f);
+
+					for (int i = 0; i < 2 * playerSize; i++) {
+						float posX2 = posX + i * playerSize * cosDegree * (playerSize * 0.2f) / playerSize;
+						float posY2 = posY - i * playerSize * sinDegree * (playerSize * 0.2f) / playerSize;
+
+						DrawPixel (posX2, posY2, playerColor);
+
+						//if (DrawPixel (posX2 + sinDegree, posY2 + cosDegree, playerColor)) {
+						//	player.Collision ();
+						//}
+
+						//for (int k = 1; k <= Mathf.CeilToInt(playerSize * 0.5f) + 2; k++) 
+						{
+							//DrawPixel (posX2 - sinDegree*k/5.0f, posY2 - cosDegree*k/5.0f, playerColor);
+						}
+					}
+				}
+
+				player.SetX (playerPosX);
+				player.SetY (playerPosY);
+			}
+		}
+	}*/
+
+
+	bool DrawPixel (float x, float y, Color col, bool collCheck)
+	{
+		int pos = (Mathf.FloorToInt(y) * arenaSize + Mathf.FloorToInt(x)) % pixMap.Length;
+
+		if (pos < 0) 
+		{
+			pos = (pixMap.Length + pos) % pixMap.Length;
+		}
+
+		if (pixMap [pos] == Color.black || pixMap [pos] == Color.green) 
+		{
+			if (collCheck) {
+				pixMap [pos] = col;
+			}
+			return false;
+		} 
+		else 
+		{
+			return true;
+		}
+	}
+
+
+	void SetArenaBgColor (Color col)
+	{
+		for (int i = 0; i < pixMap.Length; i++) 
+		{
+			pixMap [i] = col;
+		}
+
+		texture.SetPixels (pixMap);
+		texture.Apply (false);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player// : MonoBehaviour 
+{
+	private Vector2 playerPos;
+	private Color playerColor;
+
+	private int playerSize;
+	private float playerSpeed;
+
+	private float playerDegree;
+
+	private KeyCode keyLeft; 
+	private KeyCode keyRight;
+
+	private int direction = 0;
+
+	private bool active = true;
+
+	public Player ()
+	{
+		playerPos = new Vector2(0.0f, 0.0f);
+
+		playerSize 		= 4;
+		playerSpeed 	= 1.0f;
+		playerDegree 	= 0.0f;	
+
+		playerColor = Color.white;
+
+		keyLeft = KeyCode.LeftArrow;
+		keyRight = KeyCode.RightArrow;
+	}
+
+	/*public Player (Vector2 startPos, float startDeg, int size, int speed, Color col)
+	{
+		playerPos = startPos;
+
+		playerSize 		= size;
+		playerSpeed 	= speed;
+		playerDegree 	= startDeg;	
+
+		playerColor = col;
+	}*/
+
+	public void SetupPlayer(Vector2 startPos, float startDeg, int size, float speed, Color col)
+	{
+		playerPos = startPos;
+
+		playerSize 		= size;
+		playerSpeed 	= speed;
+		playerDegree 	= startDeg;	
+
+		playerColor = col;
+
+		keyLeft = KeyCode.LeftArrow;
+		keyRight = KeyCode.RightArrow;
+	}
+
+	public void Turn ()
+	{
+		if (Input.GetKey (keyLeft)) 
+		{
+			playerDegree -= 0.02f + playerSpeed * 0.002f;
+		}
+		if (Input.GetKey (keyRight)) 
+		{
+			playerDegree += 0.02f + playerSpeed * 0.002f;
+		}
+	}
+
+	public float GetX ()
+	{
+		return playerPos.x;
+	}
+
+	public float GetY ()
+	{
+		return playerPos.y;
+	}
+
+	public float GetSpeed ()
+	{
+		return playerSpeed;
+	}
+
+	public void DoubleSpeed ()
+	{
+		if (playerSpeed <= 8) {
+			playerSpeed *= 2;
+		}
+	}
+
+	public void ReduceSpeed ()
+	{
+		if (playerSpeed > 0.25f) {
+			playerSpeed /= 2.0f;
+		}
+	}
+
+	public int GetSize ()
+	{
+		return playerSize;
+	}
+
+	public float GetDegree()
+	{
+		return playerDegree;
+	}
+
+	public void SetX (float x)
+	{
+		playerPos.x = x;
+	}
+
+	public void SetY (float y)
+	{
+		playerPos.y = y;
+	}
+
+	public Color GetColor()
+	{
+		return playerColor;
+	}
+
+	public void ChangeColor(Color col)
+	{
+		playerColor = col;
+	}
+
+	public void DoubleSize()
+	{
+		if (playerSize < 4) {
+			playerSize += 1;
+		}
+		else if (playerSize >= 4 && playerSize <= 20) {
+			playerSize += 4;
+		}
+	}
+
+	public void ReduceSize()
+	{
+		if (playerSize > 4) {
+			playerSize -= 4;
+		} else if (playerSize > 1 && playerSize <= 4) {
+			playerSize -= 1;
+		}
+	}
+
+	public void SetControlKeys(KeyCode keyL, KeyCode keyR)
+	{
+		keyLeft = keyL;
+		keyRight = keyR;
+	}
+
+	public bool isActive()
+	{
+		return active;
+	}
+
+	public void Collision()
+	{
+		active = false;
+		Debug.Log ("Collided");
+	}
+}
