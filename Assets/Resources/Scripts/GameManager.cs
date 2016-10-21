f032f781aed824ca068a63caccd46dae0f2cc653
Marcin Dzierwa <microscopes.team@gmail.com>
2016-10-21 22:27:31 +0200

    Reorganization of code structure - part1

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,96 @@
+﻿/*!
+ * @file GameManager.cs
+ * @brief Contains definition of GameManager class.
+ * @author Marcin
+ */
+
+//==================================================
+//               D I R E C T I V E S
+//==================================================
+
+using UnityEngine;
+using System.Collections;
+
+//==================================================
+//                 N A M E S P A C E
+//==================================================
+
+/*!
+ * @brief A global namespace for project-scopes.
+ * @detail Contains all project-scopes related classes.
+ */
+namespace ProjectScopes
+{
+
+//==================================================
+//                    C L A S S
+//==================================================
+
+/*!
+ * @brief
+ * 
+ * @details
+ *
+ *
+ */
+
+	public class GameManager : MonoBehaviour 
+	{
+		public static GameManager instance = null;
+
+		private Configurator gameConfiguration;
+        private Level level;
+
+		void Awake ()
+		{
+			// Implementation of singleton pattern
+			if (instance == null)
+			{
+				instance = this;
+			}
+			else if (instance != this)
+			{
+				Destroy(gameObject);    
+			}
+
+			//Sets this to not be destroyed when reloading scene
+			DontDestroyOnLoad(gameObject);
+
+            //Get a component reference to the attached Level script
+            level = GetComponent<Level>();
+
+			StartLevel();
+		}
+		
+		// Update is called once per frame
+		void Update () 
+        {
+            level.MovePlayers();
+		}
+        
+        //This is called each time a scene is loaded.
+        void OnLevelWasLoaded(int index)
+        {
+            //Call StartLevel to initialize next level.
+            StartLevel();
+        }
+
+		private void StartLevel ()
+		{
+            level.SetupLevel(gameConfiguration);
+		}
+
+		public Configurator GameConfiguration
+		{
+			set
+			{
+				gameConfiguration = value;
+			}
+			get
+			{
+				return gameConfiguration;
+			}
+		}
+	}
+
+}
---
+++ Assets/Resources/Scripts/Level.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;   // List
+
+namespace ProjectScopes
+{
+    
+    public class Level : MonoBehaviour 
+    {
+        private GameObject arena;
+        private List<Player> players;
+
+        private int noOfPlayers;
+
+        // Use this for initialization
+    	void Start () 
+        {
+            players = new List<Player>();
+            noOfPlayers = 0;
+    	}
+    	
+    	// Update is called once per frame
+    	/*void Update () {
+    	
+    	}*/
+
+        public void SetupLevel(Configurator gameConf)
+        {
+            //noOfPlayers = gameConf.CurrentNoOfPlayers;
+
+            arena = Resources.Load("Prefabs/Arena", typeof(GameObject)) as GameObject;
+
+            if (arena)
+            {
+                Instantiate(arena);
+            }
+            else
+            {
+                Debug.LogError("Arena prefab not found");
+            }
+        }
+
+        public void MovePlayers()
+        {
+
+        }
+    }
+
+}
